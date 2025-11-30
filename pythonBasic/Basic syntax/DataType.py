e2b16fb0a9b55357f6665d987e9d4a7830d5a309
codersunny812 <sengersunny448@gmail.com>
2025-11-30 14:17:10 +0530

    pythn basic learned

---
+++ pythonBasic/Basic syntax/DataType.py
@@ -0,0 +1,67 @@
+# Demonstration of various data types in Python
+
+print("Demonstrating different data types in Python:")
+
+
+x= "sunny"
+print("x:", x)
+print("Type of x:", type(x))
+
+x= 20
+print("x:", x)
+print("Type of x:", type(x))
+
+x= 5.5
+print("x:", x)
+print("Type of x:", type(x))
+
+x= True
+print("x:", x)
+print("Type of x:", type(x))
+
+x= 3 + 4j
+print("x:", x)
+print("Type of x:", type(x))
+
+x= [1, 2, 3, 4, 5]
+print("x:", x)
+print("Type of x:", type(x))
+
+x = (1,2,3,4,5,6)
+print("x:",x)
+print("Type of x:", type(x))
+
+
+
+x= { "name": "John", "age": 30 }
+print("x:", x)
+print("Type of x:", type(x))
+
+x= {1, 2, 3, 4, 5}
+print("x:", x)  
+print("Type of x:", type(x))
+
+x= frozenset([1, 2, 3, 4, 5])
+print("x:", x)
+print("Type of x:", type(x))
+
+x= range(6)
+print("x:", x)
+print("Type of x:", type(x))
+
+x= bytes(5)
+print("x:", x)
+print("Type of x:", type(x))
+
+x= bytearray(5)
+print("x:", x)
+print("Type of x:", type(x))
+
+x= memoryview(bytes(5))
+print("x:", x)
+print("Type of x:", type(x))
+
+
+x= None
+print("x:", x)
+print("Type of x:", type(x))
---
+++ pythonBasic/Basic syntax/Variables.py
@@ -0,0 +1,30 @@
+
+# how to declare variables in python
+
+# declaring a variable and assigning a value to it
+x = 10
+y = 5.5
+name = "John Doe"
+is_active = True
+# printing the values of the variables
+print("x:", x) 
+print("y:", y)
+print("name:", name)
+print("is_active:", is_active)  
+
+# changing the value of a variable
+x = 20
+print("Updated x:", x)  
+# declaring multiple variables in one line
+a, b, c = 1, 2.5, "Hello"
+print("a:", a)
+print("b:", b)
+print("c:", c)
+print("hello world")
+
+
+
+# if you want to specify the data type of a variable, you can use constructors
+
+age= int(30)
+print("age:", age)
